1eecc77032ae84133d8cbd0ae7c86fa2f964b6a4
Shitiz Rajvanshi <shitiz45rajvanshi@gmail.com>
2025-09-10 21:20:26 +0530

    Using application.properties file for configuration

--- payment/src/main/java/com/example/payment/StripePaymentService.java
+++ payment/src/main/java/com/example/payment/StripePaymentService.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 
 @Component
-@ConditionalOnProperty(name = "payment.provider", havingValue = "RazorPay")
-public class RazorpayPaymentService implements PaymentService {
-    String payment = "RazorPay";
+@ConditionalOnProperty(name = "payment.provider", havingValue = "Stripe")
+public class StripePaymentService implements PaymentService {
+    String payment = "Stripe";
 
     @Override
     public String pay() {
