e0557b6f06f3158b8ab734203ff704a0e72c3a95
Shitiz Rajvanshi <shitiz45rajvanshi@gmail.com>
2025-09-20 20:49:12 +0530

    Employee Service

---
+++ rest/src/main/java/com/example/rest/service/EmployeeService.java
@@ -0,0 +1,8 @@
+package com.example.rest.service;
+
+import com.example.rest.entity.Employee;
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> saveEmployee(List<Employee> employees);
+}
---
+++ rest/src/main/java/com/example/rest/service/EmployeeServiceImpl.java
@@ -0,0 +1,21 @@
+package com.example.rest.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import com.example.rest.repository.EmployeeRepository;
+import com.example.rest.entity.Employee;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public List<Employee> saveEmployee(List<Employee> employees) {
+        employees = employeeRepository.saveAll(employees);
+        return employees;
+    }
+
+}
