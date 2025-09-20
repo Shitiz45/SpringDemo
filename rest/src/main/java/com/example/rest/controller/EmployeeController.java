a8a0a383e0da3788d6327f87de1325982912bf52
Shitiz Rajvanshi <shitiz45rajvanshi@gmail.com>
2025-09-20 20:51:24 +0530

    Employee Controller

---
+++ rest/src/main/java/com/example/rest/controller/EmployeeController.java
@@ -0,0 +1,23 @@
+package com.example.rest.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import com.example.rest.service.EmployeeService;
+import com.example.rest.entity.Employee;
+
+@RestController
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @PostMapping("/employees")
+    public List<Employee> saveEmployee(@RequestBody List<Employee> employees) {
+        System.out.println("Port 8082 is under operation.....");
+        return employeeService.saveEmployee(employees);
+    }
+
+}
